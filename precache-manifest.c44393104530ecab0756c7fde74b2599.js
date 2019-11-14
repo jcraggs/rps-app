956d0cc552437942ceb569b9a43e573df9f096c5
Jon Craggs <46655916+jcraggs@users.noreply.github.com>
2019-11-14 09:27:39 +0000

    Updates

---
+++ precache-manifest.c44393104530ecab0756c7fde74b2599.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1429c70b5ec658427e6ff96757ed2637",
+    "url": "/rps-app/index.html"
+  },
+  {
+    "revision": "6c18a475f1ba67219695",
+    "url": "/rps-app/static/css/main.9206162f.chunk.css"
+  },
+  {
+    "revision": "8e930007a18b9b7334ed",
+    "url": "/rps-app/static/js/2.22090f25.chunk.js"
+  },
+  {
+    "revision": "6c18a475f1ba67219695",
+    "url": "/rps-app/static/js/main.4d0ff33f.chunk.js"
+  },
+  {
+    "revision": "f084a4ac22a22a98d116",
+    "url": "/rps-app/static/js/runtime-main.992461a3.js"
+  },
+  {
+    "revision": "7ff010c6720e32eb68342db33320ced8",
+    "url": "/rps-app/static/media/paper.7ff010c6.png"
+  },
+  {
+    "revision": "f3c7e8fa21f584cfec5f0f3665ba3536",
+    "url": "/rps-app/static/media/rock.f3c7e8fa.png"
+  },
+  {
+    "revision": "6c9748c75680f7510b7722957d4cf754",
+    "url": "/rps-app/static/media/scissors.6c9748c7.png"
+  }
+]);

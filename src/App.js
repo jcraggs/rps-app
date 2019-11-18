adbd60d7f6f170ed96e92504c2731f613a32da1f
Jon Craggs <46655916+jcraggs@users.noreply.github.com>
2019-11-18 22:55:58 +0000

    forcing update

--- src/App.js
+++ src/App.js
@@ -60,7 +60,7 @@ class App extends React.Component {
     return (
       <div className="App">
         <header className="App-header">
-          <p>Choose Rock, Paper or Scissors</p>
+          <p>Choose Rock, Paper or Scissors:</p>
           <RPSButtons changeScoreState={this.updateScores} />
           <div id="choiceContainer">
             <p id="playerChoice">Player: {this.state.recentPlayerChoice}</p>
